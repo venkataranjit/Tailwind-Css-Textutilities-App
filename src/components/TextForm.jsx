64359967bc70b3f85547b2f99c12c024e6b2ffe4
venkataranjit <112625318+venkataranjit@users.noreply.github.com>
2024-07-15 21:19:17 +0530

    Add files via upload

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const btnClass =
-  "ml-2 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-sm shadow-sm text-base font-medium text-white bg-sky-600 hover:bg-sky-700 mt-6 float-end";
+  "ml-2 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-sm shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 mt-6 float-end";
 
 const TextForm = (props) => {
   const [textArea, settextArea] = useState("");
@@ -18,45 +18,43 @@ const TextForm = (props) => {
     props.showAlert("Cleared", "Success");
   };
   const copyHandler = () => {
-    settextArea(textArea);                                       //  in react
-    let copyText = navigator.clipboard.writeText(textArea);      //  in react
-    // let copy = document.getElementsByTagName("textarea")[0]; //  in javascript
-    // copy.select(); //  in javascript
-    // navigator.clipboard.writeText(copy.value); //  in javascript
+    // settextArea(textArea);                                       //  in react
+    // let copyText = navigator.clipboard.writeText(textArea);      //  in react
+    let copy = document.getElementsByTagName("textarea")[0]; //  in javascript
+    copy.select(); //  in javascript
+    navigator.clipboard.writeText(copy.value); //  in javascript
     props.showAlert("Copied into Memory", "Success");
   };
 
-  
   const removeExtraSpaceHandler = () => {
     settextArea(textArea.replace(/\s+/g, " "));
     props.showAlert("Extra Spaces Removed", "Success");
   };
 
-  const listenHandler = () => {
-    let listen = document.getElementsByTagName("textarea")[0].value;
-    const utterance = new SpeechSynthesisUtterance(listen);
+  const SpeechHandler = () => {
+    const utterance = new SpeechSynthesisUtterance(textArea);
     const voices = speechSynthesis.getVoices();
     utterance.voice = voices[0];
     speechSynthesis.speak(utterance);
-    props.showAlert("Speaking", "Success");
+    props.showAlert("Speech Started", "Success");
   };
 
-  const downloadHandler = (filename) => {
-    const textToSave = textArea;
-    if (textToSave.length > 0) {
-      const blob = new Blob([textToSave], { type: "text/plain" });
-      const link = document.createElement("a");
-      link.href = URL.createObjectURL(blob);
-      link.download = filename;
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-      props.showAlert("Text file downloaded", "Success");
-    } else {
-      props.showAlert("No text to download", "Failed");
-    }
+  const downloadHandler = () => {
+    // create an anchor element
+    const a = document.createElement("a");
+    // store the data in a blob
+    const file = new Blob([textArea], { type: "text/plain" });
+    // set the link of the anchor element to the blob containing the data we've stored
+    // (most browsers should automatically initiate a download because this link is a blob)
+    a.setAttribute("href", URL.createObjectURL(file));
+    // set the name of the file to be downloaded
+    a.setAttribute("download", "Your Text File.txt");
+    // 'click' the anchor element causing the download to start
+    a.click();
+    // just a clean up step
+    URL.revokeObjectURL(a.getAttribute("href"));
+    props.showAlert("Your Text File Downloaded", "Success");
   };
-
   return (
     <>
       <div className="max-w-7xl mx-auto mt-4">
@@ -69,11 +67,11 @@ const TextForm = (props) => {
             value={textArea}
             placeholder="Write your message"
             onChange={(e) => settextArea(e.target.value)}
-            className="peer relative w-full rounded border border-slate-200 px-4 py-2 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-sky-500 focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+            className="peer relative w-full rounded border border-slate-200 px-4 py-2 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-indigo-500 focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
           ></textarea>
           <label
             htmlFor="id-b03"
-            className="absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\u0000a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-sky-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+            className="absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\u0000a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-indigo-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
           >
             {props.message}
           </label>
@@ -108,20 +106,16 @@ const TextForm = (props) => {
           <button type="button" className={btnClass} onClick={LowerCaseHandler}>
             To Lower Case
           </button>
-          <button type="button" className={btnClass} onClick={listenHandler}>
-            Listen
+          <button type="button" className={btnClass} onClick={SpeechHandler}>
+            Speech
           </button>
-          <button
-            type="button"
-            className={btnClass}
-            onClick={() => downloadHandler("data.txt")}
-          >
-            Download File
+          <button type="button" className={btnClass} onClick={downloadHandler}>
+            Download
           </button>
         </div>
         <div className="mt-[80px]">
           <h3>{textArea.length > 0 ? "Preview" : ""}</h3>
-          <div className="container-fluid p-4 border border-l-8 border-sky-500 rounded-sm bg-gray-100 min-h-[100px]">
+          <div className="container-fluid p-4 border border-l-8 border-indigo-500 rounded-sm bg-gray-100 min-h-[100px]">
             <p className="text-slate-400">
               {textArea.length > 0
                 ? textArea
