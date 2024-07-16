63dc0a4d88698426381378ff9577e312b69552ae
venkataranjit <112625318+venkataranjit@users.noreply.github.com>
2024-07-16 11:25:14 +0530

    Add files via upload

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -7,17 +7,18 @@ const Alert = (props) => {
         {props.alert && (
           <div class="container mx-auto space-y-5">
             <div
-              class={`flex justify-between text-${
+              className={`flex justify-between text-${
                 props.alert.type === "Failed" ? "red" : "green"
               }-700 shadow-inner rounded p-2 bg-${
                 props.alert.type === "Failed" ? "red" : "green"
               }-200`}
             >
-              <p class="self-center w-[100%] text-center">
+              <p className="self-center w-[100%] text-center">
                 <strong>{props.alert.type} : </strong>
                 {props.alert.message}
               </p>
             </div>
+            
           </div>
         )}
       </div>
